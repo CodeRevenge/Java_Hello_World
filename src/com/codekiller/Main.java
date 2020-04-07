a0b3c20e065e7942e7a67441a8bcf8e88e478fa9
CodeRevenge <mas.arley2009@hotmail.com>
2020-04-07 13:13:01 -0500

    Initial commit

---
+++ src/com/codekiller/Main.java
@@ -0,0 +1,23 @@
+package com.codekiller;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("!Hello World, now I'm a Java Dev!");
+
+        int age = 25;
+        System.out.println("Mi age is: " + age);
+
+        float height = 1.70f;
+        System.out.println("Mi height is: " + height);
+
+        double piValue = Math.PI;
+        System.out.println("The value of PI is: " + piValue);
+
+        long distanceToSun = 471628376481726846L;
+        System.out.println("The distance to the Sun is: " + distanceToSun);
+
+        boolean isDay = true;
+        System.out.println("It is day:");
+    }
+}
